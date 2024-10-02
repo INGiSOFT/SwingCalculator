9b4cdd87d74a97d455ac7c5439cea55ba4477015
Thomas <thomas.ingold@gmx.ch>
2024-10-02 13:35:58 +0200

    Refactoring.

--- src/Frame.java
+++ src/Frame.java
@@ -9,6 +9,8 @@ public class Frame extends JFrame {
 
     private ActionListener buttonListener = new ButtonListener();
 
+    private final Button[] numberButtons = new Button[10];
+
     public Frame() {
 
         setTitle("Calculator");
@@ -24,52 +26,18 @@ public class Frame extends JFrame {
         Button buttonMultiply = new Button("*");
         Button buttonDivide = new Button("/");
 
-        Button button0 = new Button("0");
-        button0.addActionListener(buttonListener);
+        for (int i = 0; i < numberButtons.length; i++) {
 
-        Button button1 = new Button("1");
-        button1.addActionListener(buttonListener);
-
-        Button button2 = new Button("2");
-        button2.addActionListener(buttonListener);
-
-        Button button3 = new Button("3");
-        button3.addActionListener(buttonListener);
-
-        Button button4 = new Button("4");
-        button4.addActionListener(buttonListener);
-
-        Button button5 = new Button("5");
-        button5.addActionListener(buttonListener);
-
-        Button button6 = new Button("6");
-        button6.addActionListener(buttonListener);
-
-        Button button7 = new Button("7");
-        button7.addActionListener(buttonListener);
-
-        Button button8 = new Button("8");
-        button8.addActionListener(buttonListener);
-
-        Button button9 = new Button("9");
-        button9.addActionListener(buttonListener);
+            numberButtons[i] = new Button(String.valueOf(i));
+            numberButtons[i].addActionListener(buttonListener);
+            buttonPanel.add(numberButtons[i]);
+        }
 
         Button buttonDot = new Button(".");
         JButton buttonEqual = new Button("=");
         buttonEqual.setBackground(Color.BLUE);
 
         buttonPanel.setLayout(new GridLayout(4,4));
-
-        buttonPanel.add(button0);
-        buttonPanel.add(button1);
-        buttonPanel.add(button2);
-        buttonPanel.add(button3);
-        buttonPanel.add(button4);
-        buttonPanel.add(button5);
-        buttonPanel.add(button6);
-        buttonPanel.add(button7);
-        buttonPanel.add(button8);
-        buttonPanel.add(button9);
 
         buttonPanel.add(buttonPlus);
         buttonPanel.add(buttonMinus);
